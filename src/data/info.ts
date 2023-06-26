39db8c15eebc0fcb02f817a9d053edaccbb46bae
Alex Korn <akorndev@gmail.com>
2023-06-26 13:31:40 -0500

    init
    init

---
+++ src/data/info.ts
@@ -0,0 +1,142 @@
+export const info = {
+  baseUrl: "https://akorn-portfolio.netlify.app",
+  name: "Alex Korn",
+  jobDescription: "Full-Stack Software Engineer",
+  about: `I am a full-stack software engineer and technical leader with over 5 years of experience, 
+  I have a strong foundation in software engineering principles and a passion for building scalable, 
+  innovative software solutions. I have experience working with various technologies and frameworks, 
+  including AWS and Azure, React, C#, Java, Python, GO, Rust and TypeScript. I am skilled in the frontend, backend, 
+  and in cloud development, and I have a track record of delivering successful projects on time and to specification. 
+  I am a proactive problem-solver with excellent communication and collaboration skills, 
+  and I am always looking for opportunities to learn and grow as a leader and software engineer.
+  `,
+
+  experience: [
+    {
+      name: "Slalom - Software Engineering Consultant",
+      location: "Austin, TX (remote)",
+      startDate: "March 2022",
+      endDate: "Present",
+      description: [
+        "-  Collaborated with clients to understand their business needs and translate them into technical requirements.",
+        "-  Lead cross-functional teams to deliver successful software projects on time and to specification.",
+        "-  Designed and implemented software solutions using a variety of programming languages and frameworks.",
+        "-  Conducted technical assessments and provided recommendations for improving software processes and systems.",
+        "-  Typescript, C#, Go, Python, React, Next JS, Terraform, AWS, Azure, Git, Okta, gRPC, REST",
+      ],
+    },
+    {
+      name: "Discover Financial Services - Senior Software Engineer",
+      location: "Austin, TX (remote)",
+      startDate: "July 2021",
+      endDate: "March 2022",
+      description: [
+        "-  Designed, developed, and tested REST APIs and implemented user interface functionality.",
+        "-  Set code standards/guidelines and developed CI/CD infrastructure.",
+        "-  Successfully launched beta and MVP versions of the product.",
+        "-  Typescript, React, Express, Postgres, Node, Redis, Jenkins, OpenShift, AWS, Git, Github Actions, Elasticsearch",
+      ],
+    },
+    {
+      name: "Cox Automotive - Software Engineer II",
+      location: "Austin, TX (remote)",
+      startDate: "Oct 2020",
+      endDate: "July 2021",
+      description: [
+        "-  Designed, developed, and tested REST APIs and implemented user interface functionality",
+        "-  Architected end-to-end automation workflows for continuous integration and deployment.",
+        "-  C#, .NET, Typescript, AWS, New Relic, Storybook, React, DynamoDB",
+      ],
+    },
+    {
+      name: "Beckman Coulter - Software Engineer II",
+      location: "Chaska, MN",
+      startDate: "Jan 2019",
+      endDate: "Oct 2020",
+      description: [
+        "-  Developed, designed, and tested REST APIs and implemented user interface functionality.",
+        "-  Successfully set up Azure infrastructure, VMs, Functions, and Azure DevOps CI/CD pipelines.",
+        "-  C#, .NET, GIT, SQL Server, PowerShell, WPF, Wix, Azure, AWS, Azure DevOps, Razor Pages, JavaScript",
+      ],
+    },
+    {
+      name: "Genesis 10 - Java Developer",
+      location: "Minneapolis, MN",
+      startDate: "Jun 2018",
+      endDate: "Dec 2018",
+      description: [
+        "-  Developed REST APIs for internal users to access data from many sources.",
+        "-  Helped modernize Ameriprise infrastructure using Docker and Kubernetes.",
+        "-  Java, Spring, Elastic search, Scala, Git, Maven, Docker, Kubernetes.",
+      ],
+    },
+    {
+      name: "Beckman Coulter - Software Engineering Intern",
+      location: "Chaska, MN",
+      startDate: "May 2017",
+      endDate: "Aug 2017",
+      description: [
+        "-  Designed and implemented a troubleshooting system",
+        "-  C#, .NET, Wix, Share Point, SQL Server, Azure",
+      ],
+    },
+  ],
+
+  skills: [
+    {
+      title: "Certifications",
+      skills: "AWS Solutions Architect",
+    },
+    {
+      title: "Languages",
+      skills: "Typescript, C#, Python, Go, Java, Rust, SQL",
+    },
+    {
+      title: "Frameworks",
+      skills: "React, Next.js, Node.js, Flask, .NET, Spring, Pandas, Tailwind",
+    },
+    {
+      title: "Databases",
+      skills:
+        "DynamoDB, MongoDB, Postgres, SQL Server, Redis, MySQL, Snowflake",
+    },
+    {
+      title: "Skills",
+      skills:
+        "Git, Docker, Kubernetes. Terraform, gRPC, REST, AWS, Azure, Vercel, Okta, Linux, Windows, OS X",
+    },
+  ],
+
+  education: [
+    {
+      name: "Gustavus Adolphus College",
+      location: "St. Peter, MN",
+      startDate: "2014",
+      endDate: "2018",
+      description: ["Bachelors in Computer Science"],
+    },
+    {
+      name: "Eden Prairie High School",
+      location: "Eden Prairie",
+      startDate: "2010",
+      endDate: "2014",
+      description: ["High School Diploma"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/AlexSKorn",
+    email: "alexkorn1122@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alex-korn-97510a11b/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+export interface IMetaHead {
+  title: string;
+  description: string;
+  ogImageUrl: string;
+}
+
+export interface IHeroProps {
+  name: string;
+  about: string;
+}
+
+export interface IExperience {
+  name: string;
+  jobTitle: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  description: string[];
+}
+export interface IExperiences {
+  title: string;
+  details: IExperience[];
+}
+
+export interface ITechnicalSkills {
+  title: string;
+  skills: ITechSkill[];
+}
+
+export interface ITechSkill {
+  title: string;
+  skills: string;
+}
+
+export interface IProject {
+  title: string;
+  isFeatured: boolean;
+  thumbnail: string;
+  githubUrl: string;
+  liveUrl: string;
+}
+export interface IProjects {
+  projects: IProject[];
+}
+
+export interface IProjectDetails {
+  projectDetail: IProject;
+}
